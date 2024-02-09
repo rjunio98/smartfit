5cb2b6c55596031d0cf88b6296ba1778f3f73d97
rjunio98 <robledorjunior1@gmail.com>
2024-02-09 13:38:17 -0300

    fat: add labels constants

---
+++ src/constants/labels.ts
@@ -0,0 +1,78 @@
+interface LabelImage {
+  url: string;
+  alt: string;
+  description: string;
+}
+
+interface Label {
+  description: string;
+  images: LabelImage[];
+}
+
+export const LABELS: Label[] = [
+  {
+    description: "Máscara",
+    images: [
+      {
+        url: "/required-mask.png",
+        alt: "Obrigatória",
+        description: "Obrigatória",
+      },
+      {
+        url: "/recommended-mask.png",
+        alt: "Recomendada",
+        description: "Recomendada",
+      },
+    ],
+  },
+  {
+    description: "Toalha",
+    images: [
+      {
+        url: "/required-towel.png",
+        alt: "Obrigatória",
+        description: "Obrigatória",
+      },
+      {
+        url: "/recommended-towel.png",
+        alt: "Recomendada",
+        description: "Recomendada",
+      },
+    ],
+  },
+  {
+    description: "Bebedouro",
+    images: [
+      {
+        url: "/partial-fountain.png",
+        alt: "Parcial",
+        description: "Parcial",
+      },
+      {
+        url: "/forbidden-fountain.png",
+        alt: "Proibido",
+        description: "Proibido",
+      },
+    ],
+  },
+  {
+    description: "Vestiários",
+    images: [
+      {
+        url: "/required-lockerroom.png",
+        alt: "Liberado",
+        description: "Liberado",
+      },
+      {
+        url: "/partial-lockerroom.png",
+        alt: "Paricial",
+        description: "Parcial",
+      },
+      {
+        url: "/forbidden-lockerroom.png",
+        alt: "Fechado",
+        description: "Fechado",
+      },
+    ],
+  },
+];
