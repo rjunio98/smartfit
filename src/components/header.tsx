5f911c2825bfa251ab21da20c318d920c4bd630b
rjunio98 <robledorjunior1@gmail.com>
2024-02-09 10:12:04 -0300

    refactor: ensure Header component uses header html tag

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <div className="bg-black flex items-center justify-center p-5">
+    <header className="bg-black flex items-center justify-center p-5">
       <img
         width={180}
         src="/logo.svg"
         alt="logo"
       />
-    </div>
+    </header>
   );
 };
 
