9d05fc69c2aa9ab8b2a9e3812a9e10b5dd997101
rjunio98 <robledorjunior1@gmail.com>
2024-02-09 05:03:04 -0300

    feat: add Welcome component

---
+++ src/components/welcome.tsx
@@ -0,0 +1,20 @@
+const Welcome = () => {
+  return (
+    <div className="py-12 px-5">
+      <h1 className="text-2xl font-bold uppercase mb-6">
+        Reabertura <br />
+        Smart Fit
+      </h1>
+
+      <div className="h-4 w-36 bg-brand-darkGrey mb-8" />
+
+      <p>
+        O horário de funcionamento das nossas unidades está seguindo os decretos
+        de cada município. Por isso, confira aqui se a sua unidade está aberta e
+        as medidas de segurança que estamos seguindo.
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
